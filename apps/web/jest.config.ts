5bbbb329f67eb19916edb9527fa2018760445eb2
Mario Adisurya <mario.adisurya@gmail.com>
2022-08-11 22:25:57 +1200

    chore: `@geapot-web` -> `web`

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: '@geapot-web',
+  displayName: 'web',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/@geapot-web',
+  coverageDirectory: '../../coverage/apps/web',
 };
