b81a4eee8f0384ba98ae42763ecd08b94c5dc42d
Mario Adisurya <mario.adisurya@gmail.com>
2022-08-13 14:08:20 +1200

    fix: formatting

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -8,7 +8,9 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <StrictMode>
     <App />
