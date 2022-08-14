3620bd80d8ee582f3051771f26b6ff92e849f191
NatRivers <tnathania6@gmail.com>
2022-08-14 21:09:41 +1200

    chore: :lipstick: cleanup

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,22 +1,10 @@
-import styled from '@mui/styled-engine';
-import { Typography } from '@mui/material';
-
-import NxWelcome from './nx-welcome';
 import { ThemeProvider } from './providers';
-
-const StyledApp = styled('div')`
-  // Your style here
-`;
+import Home from './pages/home';
 
 export function App() {
   return (
     <ThemeProvider>
-      <StyledApp>
-        <Typography variant="h2" color="primary.main" align="center" p={2}>
-          Header with theming
-        </Typography>
-        <NxWelcome title="geapot-web" />
-      </StyledApp>
+      <Home />
     </ThemeProvider>
   );
 }
